03f3ba8e5af5ab0516a1b8fff992e755b1cba743
Seem@1330 <seemakhutwad90@gmail.com>
2025-05-13 11:55:26 +0530

    added react hook form and create signup and login pages from input and button components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,16 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
-import Logo from "./Logo/Logo";
-import LogoutBtn from "./LogoutBtn/LogoutBtn";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import AuthLayout from "./AuthLayout";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Container from "./Container";
+import Select from "./Select";
 
-export { Header, Footer,Container, Logo, LogoutBtn };
+
+export { Header, Footer,Container, Logo, LogoutBtn, Button, Input, AuthLayout, SignUp, Login,PostCard,Container, Select };
