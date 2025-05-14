e8a2705b16178743bbacdc6644cc746d1eedc19f
Seem@1330 <seemakhutwad90@gmail.com>
2025-05-14 17:44:04 +0530

    created pages for all required components

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, PostForm } from '../Components'
+
+function AddPost() {
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <PostForm/>
+        </Container>
+        </div>
+  )
+}
+
+export default AddPost
