5e39c70a4b11ba6741e200f0653443ff7fdd8220
Seem@1330 <seemakhutwad90@gmail.com>
2025-05-14 17:44:29 +0530

    added routing in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
 import { useDispatch } from 'react-redux';
-import { authService } from './appwrite/auth.js';
+import  authService  from './appwrite/auth';
 import { login, logout } from './store/authSlice';
 import { Footer, Header } from './Components/index.js';
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
-  // console.log(import.meta.env.VITE_APPWRITE_URL);
 
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
@@ -15,13 +16,11 @@ function App() {
     authService.getCurrentUser()
       .then((userData) => {
         if (userData) {
-          dispatch(login(userData));
+          dispatch(login({userData}));
         } else {
           dispatch(logout());
         }
-      }).finally(() => {
-        setLoading(false);
-      })
+      }).finally(() => setLoading(false))
   }, [])
 
   // if (loading) {
@@ -44,7 +43,7 @@ function App() {
       <div className='w-full-block'>
         <Header/>
       <main>
-       TODO: {/* <Outlet/> */}
+       TODO: <Outlet/>
       </main>
       <Footer/>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,79 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import  store  from './store/store.js'
+import  store  from './store/store'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AuthLayout } from './Components/index.js'
+import Home from './pages/Home'
+import AllPosts from './pages/AllPosts'
+import SignUp from './pages/SignUp'
+import AddPost from './pages/AddPost'
+import Login from './pages/Login'
+import EditPost from './pages/EditPost'
+import Post from './pages/Post'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Login />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <SignUp />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPosts />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AddPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
+    ],
+},
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-    <App />
+    <Provider  store={store}>
+    <RouterProvider router={router}/>
     </Provider>
-  </StrictMode>,
 )
